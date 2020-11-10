c5008beed650ed7a6b046f6762d9460d41329fc2
John Ruane <john.ruane@jdwilliams.co.uk>
2020-11-10 16:34:09 +0000

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>John Ruane Portfolio</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+
+      </main>
+
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
