13dee21b692abf62e7afc814457531529f12c725
John Ruane <john.ruane@jdwilliams.co.uk>
2020-11-13 22:42:51 +0000

    new banner images

--- pages/index.js
+++ pages/index.js
@@ -26,9 +26,9 @@ export default function Home() {
         </div>
         <img className={styles.profileImage} src="/images/profile.png"/>
         <div className={styles.profileTextWrapper}>
-          <p>Hi. I'm John Ruane and I am a frontend developer currently working at N Brown Group in Manchester.</p>
+          <p className={styles.profileText}>Hi. I'm John Ruane and I am a frontend developer currently working at N Brown Group in Manchester.</p>
           <br/>
-          <p>I have 5+ years experience designing and building UI, and coding with Javascript. Most recently I am 
+          <p className={styles.profileText}>I have 5+ years experience designing and building UI, and coding with Javascript. Most recently I am 
             developing with React and a number of other technologies such as Node, Redux, Typescript, Jest & REST APIs.</p>
         </div>
         <Footer/>
