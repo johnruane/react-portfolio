d00907d0057a277495319b60fa119b5de3032e91
John Ruane <john.ruane@jdwilliams.co.uk>
2021-01-12 22:54:08 +0000

    Integrate contentful CMS

--- pages/article/[page].js
+++ pages/article/[page].js
@@ -1,61 +1,66 @@
-import Layout from '../../components/Layout';
-import Pill from '../../components/Pill';
-import TextBanner from '../../components/TextBanner';
-import ArticleText from '../../components/ArticleText';
-import ArticleBlock from '../../components/ArticleBlock';
-import style from '../../styles/Article.module.css'
-import data from '../../data/articles.json';
+import Layout from "../../components/Layout";
+import Pill from "../../components/Pill";
+import TextBanner from "../../components/TextBanner";
+import ArticleText from "../../components/ArticleText";
+import ArticleBlock from "../../components/ArticleBlock";
+import style from "../../styles/Article.module.css";
+import data from "../../data/articles.json";
 
-const ArticlePage = ({
-  data
-}) => {
+const ArticlePage = ({ data, data2 }) => {
   const title = data.intro;
   const body = data.body;
+  const content = data2.items[0].fields;
+
   return (
     <Layout
       className="articleCoverSlot"
-      coverSlot={     
-        <>   
+      coverSlot={
+        <>
           <article className={style.article}>
-            <div className={style.supHeadingText}>Responsive</div>
-            <div className={style.headingText}>SimplyBe Int'l</div>
-            <div className={style.subHeadingText}>Released 2003</div>
+            <div className={style.supHeadingText}>{content.subHeading}</div>
+            <div className={style.headingText}>{content.heading}</div>
+            <div className={style.subHeadingText}>
+              {`Released ${content.released}`}
+            </div>
           </article>
           <div className={style.subFooterWrapper}>
             <div className={style.subFooterHeadingText}>Technology</div>
-            <Pill items={"Responsive, LESS, Wireframing, Prototyping, Flow diagrams, Useability testing, Requirements gathering, Card sorting, Grunt, Templating, Agile, Hybris"} />
+            <Pill items={content.techList.join(",")} />
           </div>
         </>
       }
     >
-      <ArticleText
-        title="Intro"
-        text={title}
+      <ArticleText title="Intro" text={title} />
+      <img
+        className={style.articleImage}
+        src="https://via.placeholder.com/750x548.png"
       />
-      <img className={style.articleImage} src="https://via.placeholder.com/750x548.png"/>
-      <TextBanner text="“Shared codebase across all devices on the Hybris Commerce platform."/>
+      <TextBanner text="“Shared codebase across all devices on the Hybris Commerce platform." />
       <ArticleBlock>
         {body.map((content) => {
-          return (
-            <ArticleText
-              title={content.heading}
-              text={content.text}
-            />          
-          )
+          return <ArticleText title={content.heading} text={content.text} />;
         })}
       </ArticleBlock>
-      <img className={style.articleImage} src="https://via.placeholder.com/750x548.png"/>
+      <img
+        className={style.articleImage}
+        src="https://via.placeholder.com/750x548.png"
+      />
     </Layout>
-  )
-}
+  );
+};
 
 export const getServerSideProps = async ({ query }) => {
   const pageText = data[query.page];
-  return { 
+  const res = await fetch(
+    `${process.env.CONTENTFUL_URL}/spaces/${process.env.CONTENTFUL_SPACE_ID}/entries?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}`
+  );
+  const data2 = await res.json();
+  return {
     props: {
       data: pageText,
-    }
-  }
-}
+      data2,
+    },
+  };
+};
 
 export default ArticlePage;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Layout from '../components/Layout';
-import PreviewCard from '../components/PreviewCard';
-import style from '../styles/Home.module.css'
+import Head from "next/head";
+import Layout from "../components/Layout";
+import PreviewCard from "../components/PreviewCard";
+import style from "../styles/Home.module.css";
 
-const Home = () => {
+export default Home = () => {
   return (
     <div>
       <Head>
@@ -11,10 +11,12 @@ const Home = () => {
       </Head>
 
       <Layout
-        coverSlot={        
+        coverSlot={
           <article className={style.salutationWrapper}>
             <div className={style.salutationText}>Hello,</div>
-            <div className={style.salutationSubText}>I'm a frontend developer from Manchester.</div>
+            <div className={style.salutationSubText}>
+              I'm a frontend developer from Manchester.
+            </div>
           </article>
         }
       >
@@ -55,17 +57,31 @@ const Home = () => {
 
         <article className={`grid ${style.aboutContent}`}>
           <div className={style.aboutImageWrapper}>
-            <img className={style.aboutImage} src="/images/profile.png"/>           
+            <img className={style.aboutImage} src="/images/profile.png" />
           </div>
           <h2 className={style.aboutHeading}>A bit about me</h2>
-          <p className={style.aboutText}>I am a frontend developer working at N Brown Group in Manchester and have 10+ years designing and building user interfaces for the web, specifically for ecommerce. Web accessibility and performance are at the forefront of every project I work on - delivering an amazing experience to all customer across all devices.<br /><br /> 
-        Most recently I have moved away from UI design into a more full stack development role - CI/CD, integration testing & docker. I have been developing frontends with React and a number of other technologies such as Node, Redux, Typescript, Jest & REST APIs.<br /><br />
-        Through the purpose of what and who our ecommerce websites are for I have built up strong skills in web accessibility and performance.<br /><br />
-        I have a BSc honors degree in Computing from Manchester Metropolitan University.</p>
+          <p className={style.aboutText}>
+            I am a frontend developer working at N Brown Group in Manchester and
+            have 10+ years designing and building user interfaces for the web,
+            specifically for ecommerce. Web accessibility and performance are at
+            the forefront of every project I work on - delivering an amazing
+            experience to all customer across all devices.
+            <br />
+            <br />
+            Most recently I have moved away from UI design into a more full
+            stack development role - CI/CD, integration testing & docker. I have
+            been developing frontends with React and a number of other
+            technologies such as Node, Redux, Typescript, Jest & REST APIs.
+            <br />
+            <br />
+            Through the purpose of what and who our ecommerce websites are for I
+            have built up strong skills in web accessibility and performance.
+            <br />
+            <br />I have a BSc honors degree in Computing from Manchester
+            Metropolitan University.
+          </p>
         </article>
       </Layout>
     </div>
-  )
-}
-
-export default Home;
+  );
+};
